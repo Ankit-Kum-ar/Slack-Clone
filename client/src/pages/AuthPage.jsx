52cbaee050dc79f4a100e510bc66f9f31b3f003e
Ankit-0009 <ankitcode2511@gmail.com>
2025-09-09 23:50:56 +0530

    feat: add routing and authentication pages; update dependencies

---
+++ client/src/pages/AuthPage.jsx
@@ -0,0 +1,61 @@
+import { SignInButton } from '@clerk/clerk-react'
+import '../styles/auth.css'
+import React from 'react'
+
+const AuthPage = () => {
+  return (
+    <div className='auth-container'>
+        <div className="auth-left">
+            <div className='auth-hero'>
+                <div className='brand-container'>
+                    <img src="/logo.png" alt="Slack Logo" className='brand-logo' />
+                    <span className='brand-name'>Slap</span>
+                </div>
+
+                <h1 className='hero-title'>Where Work Happens ✨</h1>
+
+                <p className='hero-subtitle'>
+                    Connect with your team, collaborate on projects, and get things done in one place. Experenice the new way to work together.
+                </p>
+
+                <div className='features-list'>
+                    <div className='feature-item'>
+                        <span className='feature-icon'>💬</span>
+                        <span className='feature-text'>Real-time Messaging</span>
+                    </div>
+
+                    <div className='feature-item'>
+                        <span className='feature-icon'>📸</span>
+                        <span className='feature-text'>Video calls & meetings</span>
+                    </div>
+
+                    <div className='feature-item'>
+                        <span className='feature-icon'>🔒</span>
+                        <span className='feature-text'>Secure and Private</span>
+                    </div>
+                </div>
+
+                <SignInButton mode="modal">
+                    <button className='cta-button'>
+                        Get Started with Slap
+                        <span className='button-arrow'>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-arrow-right" viewBox="0 0 16 16">
+                                <path fillRule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"/>
+                            </svg>
+                        </span>
+                    </button>
+                </SignInButton>
+            </div>
+        </div>
+
+        <div className="auth-right">
+            <div className='auth-image-container'>
+                <img src="/auth-i.png" alt="Auth Hero" className='auth-image' />
+                <div className='image-overlay'></div>
+            </div>
+        </div> 
+    </div>
+  )
+}
+
+export default AuthPage
