29dc12cf67f69b8bfceb062584d084ad05fe21ea
Ankit-0009 <ankitcode2511@gmail.com>
2025-09-15 00:40:35 +0530

    feat: integrate Tailwind CSS and add RippleWaveLoader component
    - Added Tailwind CSS and its Vite plugin to the project.
    - Introduced RippleWaveLoader component for loading state.
    - Updated App component to use useAuth hook for authentication state.
    - Implemented Stream Chat integration with useStreamChat hook.
    - Created API function to fetch Stream Chat token.
    - Enhanced HomePage with UserButton from Clerk.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,42 @@
-import React from "react";
-import { SignedIn, SignedOut } from "@clerk/clerk-react";
+import { useAuth } from "@clerk/clerk-react";
 import * as Sentry from "@sentry/react";
 import { Navigate, Route, Routes } from "react-router";
-import HomePage from "./pages/HomePage";
 import AuthPage from "./pages/AuthPage";
+import HomePage from "./pages/HomePage";
+import RippleWaveLoader from "./components/RippleWaveLoader";
 
 const SentryRoutes = Sentry.withSentryReactRouterV7Routing(Routes);
 const App = () => {
+	const { isSignedIn, isLoaded } = useAuth();
+	if (!isLoaded) {
+		return (
+			<div className="h-screen w-screen flex items-center justify-center">
+				<RippleWaveLoader />
+			</div>
+		);
+	}
 	return (
 		<>
-			<SignedIn>
+			<SentryRoutes>
+				<Route
+					path="/"
+					element={
+						isSignedIn ? <HomePage /> : <Navigate to={"/auth"} replace />
+					}
+				/>
+				<Route
+					path="/auth"
+					element={isSignedIn ? <Navigate to={"/"} replace /> : <AuthPage />}
+				/>
+			</SentryRoutes>
+		</>
+	);
+};
+
+export default App;
+
+// {
+/* <SignedIn>
 				<SentryRoutes>
 					<Routes>
 						<Route path="/" element={<HomePage />} />
@@ -25,9 +52,5 @@ const App = () => {
 						<Route path="*" element={<Navigate to={"/auth"} replace />} />
 					</Routes>
 				</SentryRoutes>
-			</SignedOut>
-		</>
-	);
-};
-
-export default App;
+			</SignedOut> */
+// }
--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,9 +1,11 @@
+import { UserButton } from '@clerk/clerk-react'
 import React from 'react'
 
 const HomePage = () => {
   return (
     <div>
       HomePage
+      <UserButton />
     </div>
   )
 }
